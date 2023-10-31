e3cf19f895353901b7b12f55219804d173f22da2
rui-min <minruiray@outlook.com>
2023-10-30 21:11:45 -0400

    Get from page.js directly

--- src/app/backend/api/catalog/route.js
+++ src/app/backend/api/catalog/route.js
@@ -1,27 +1,23 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../db/dbClient";
+import { prisma } from "../../db/dbClient.js";
 
 // To handle a GET request to /api
 export async function GET(request, {params}) {
   const slug = params.slug
   // Do whatever you want
-  const res = await fetch(env("DATABASE_URL"), {
-    next: { revalidate: 60 }, // Revalidate every 60 seconds
-  })
-  const data = await res.json()
+  const vehicles = await prisma.vehicle.findMany();
 
   // return Response.json(data)
   redirect('https://nextjs.org/')
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  return NextResponse.json(vehicles, { status: 200 });
 }
 
 // To handle a POST request to /api
 export async function POST(request) {
   // Do whatever you want
+  const json = await request.json();
   const vehicle = await prisma.vehicle.create({
-    data: {
-
-    },
+    data: json,
   })
   return NextResponse.status(201).json(vehicle);
 }
