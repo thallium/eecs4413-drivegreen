a8f5907b2630dad69e95c37b3454f8d529a1a7b6
rui-min <minruiray@outlook.com>
2023-11-01 11:15:51 -0400

    Made vehicles get method

--- src/app/page.js
+++ src/app/page.js
@@ -9,34 +9,10 @@ export default async function Home() {
         Read <Link href="/viewRoute1/">this Route1 page!</Link>
         <br></br>
         Read <Link href="/viewRoute2/">this Route2 page!</Link>
+        <br></br>
+        <Link href="/vehicles/">Vehicles page</Link>
       </div>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr 1fr",
-          gap: 20,
-        }}
-      >
-        {vehicles.map((vehicle) => (
-          <div
-            key={vehicle.vid}
-            style={{ border: "1px solid #ccc", textAlign: "center" }}
-          >
-            {/* <img
-              src={`https://robohash.org/${vehicle.vid}?set=set2&size=180x180`}
-              alt={vehicle.name}
-              style={{ height: 180, width: 180 }}
-            /> */}
-            <h3>{JSON.stringify(vehicle.createdAt)}</h3>
-            <h3>{vehicle.name}</h3>
-            <h3>{vehicle.brand}</h3>
-            <h3>{vehicle.shape}</h3>
-            <h3>{vehicle.modelYear}</h3>
-            <h3>{JSON.stringify(vehicle.hotDealed)}</h3>
-          </div>
-        ))}
-      </div>
+      
     </main>
   )
 }
---
+++ src/app/vehicles/page.js
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+
+let vehicles = await fetch(
+    (process.env.NODE_ENV !== 'production'? process.env.LOCAL_URL : process.env.VERCEL_URL) + "/api/vehicles"
+      ,{ cache: 'no-store' })
+    .then(res=>res.json().then(data =>{ return data; }) );
+
+export default async function ListVehicles() {
+  return (
+    <>
+      <h1>First Route</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          gap: 20,
+        }}
+      >
+        {vehicles.map((vehicle) => (
+          <div
+            key={vehicle.vid}
+            style={{ border: "1px solid #ccc", textAlign: "center" }}
+          >
+            {/* <img
+              src={`https://robohash.org/${vehicle.vid}?set=set2&size=180x180`}
+              alt={vehicle.name}
+              style={{ height: 180, width: 180 }}
+            /> */}
+            <h3>{JSON.stringify(vehicle.createdAt)}</h3>
+            <h3>{vehicle.name}</h3>
+            <h3>{vehicle.brand}</h3>
+            <h3>{vehicle.shape}</h3>
+            <h3>{vehicle.modelYear}</h3>
+            <h3>{JSON.stringify(vehicle.hotDealed)}</h3>
+          </div>
+        ))}
+      </div>
+
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
