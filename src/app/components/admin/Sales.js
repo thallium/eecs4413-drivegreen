94e15844e57b698e4e1a79464c118aac7b19a7da
sss <10131931+sowiesss@users.noreply.github.com>
2023-11-01 22:24:36 -0400

    try again

--- src/app/components/admin/Sales.js
+++ src/app/components/admin/Sales.js
@@ -4,7 +4,7 @@ export default function Sales(props) {
     //get order list from api for this month
 
     return <>
-    {props && <h1>{props.res.message}</h1>}
-    {!props && <h1>null...</h1>}
+    {props.res && <h1>{props.res.message}</h1>}
+    {!props.res && <h1>null...</h1>}
     </>;
 }
