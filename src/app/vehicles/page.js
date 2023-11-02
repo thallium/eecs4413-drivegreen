df563fb618e15319a6f5a01f0d64da29b544b1b6
rui-min <minruiray@outlook.com>
2023-11-02 19:54:36 -0400

    no cache for page

--- src/app/vehicles/page.js
+++ src/app/vehicles/page.js
@@ -1,9 +1,10 @@
-import Link from 'next/link';
+// "use client"; // This is a client component
 
+import Link from 'next/link';
 
 export default async function ListVehicles() {
   const getAPI = (process.env.NODE_ENV !== 'production'? process.env.LOCAL_URL : "https://" + process.env.VERCEL_URL) + "/api/vehicles";
-  let vehicles = await fetch(getAPI, { cache: 'force-cache' })
+  let vehicles = await fetch(getAPI, { cache: 'no-store' })
       .then(res=>res.json().then(data =>{ return data; }))
       .catch(err => console.log(err));
 
