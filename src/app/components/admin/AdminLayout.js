876ee6c353305c08f0c77bf9cc717673a657a506
sss <10131931+sowiesss@users.noreply.github.com>
2023-11-01 22:39:45 -0400

    display vercel url

--- src/app/components/admin/AdminLayout.js
+++ src/app/components/admin/AdminLayout.js
@@ -5,25 +5,22 @@ import HotDealManage from './HotDealManage';
 
 
 export default async function AdminLayout() {
-  // console.log(process.env.VERCEL_URL, process.env.NODE_ENV);
-  // const res = await fetch(process.env.LOCAL_URL + '/api/admin', {
-  //   method: 'GET',
-  // }).catch((err) => console.log(err));
-  console.log(process.env.VERCEL_URL);
-  // const res = await fetch(process.env.VERCEL_URL + '/api/admin', {
-  //   method: 'GET',
-  // })
-  //   .then((res) => {
-  //     return res.json();
-  //   })
-  //   .catch((err) => console.log(err));
 
-  // console.log(res);
+
+  const res = await fetch(process.env.VERCEL_URL + '/api/admin', {
+    method: 'GET',
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+
+  console.log(res);
 
   return (
     <>
-
-      {/* <Sales res={res} /> */}
+      {process.env.VERCEL_URL && <h1>VERCEL_URL: {process.env.VERCEL_URL}</h1>}
+      <Sales res={res} />
 
       <LoginHistory />
 
