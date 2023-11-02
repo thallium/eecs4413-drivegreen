4007ee50c4fe59987d31347366719f316be791da
sss <10131931+sowiesss@users.noreply.github.com>
2023-11-01 20:44:34 -0400

    try local url

--- src/app/components/admin/AdminLayout.js
+++ src/app/components/admin/AdminLayout.js
@@ -5,11 +5,14 @@ import HotDealManage from './HotDealManage';
 
 
 export default async function AdminLayout() {
-  console.log(process.env.VERCEL_URL, process.env.NODE_ENV);
+  // console.log(process.env.VERCEL_URL, process.env.NODE_ENV);
+  // const res = await fetch(process.env.LOCAL_URL + '/api/admin', {
+  //   method: 'GET',
+  // }).catch((err) => console.log(err));
 
-  const res = await fetch(process.env.VERCEL_URL + '/api/admin', {
+  const res = await fetch(process.env.LOCAL_URL + '/api/admin', {
     method: 'GET',
-  }).catch(err => console.log(err));
+  }).catch((err) => console.log(err));
 
   const data = await res.json();
 
