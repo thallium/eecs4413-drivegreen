3db47daa88b5074b652ab32fc48368b6e6de8044
sss <10131931+sowiesss@users.noreply.github.com>
2023-11-01 23:17:27 -0400

    try again

--- src/app/components/admin/AdminLayout.js
+++ src/app/components/admin/AdminLayout.js
@@ -10,6 +10,7 @@ export default async function AdminLayout() {
       ? process.env.LOCAL_URL
       : 'https://' + process.env.VERCEL_URL;
 
+  console.log('url: ' + url);
   const res = await fetch(url + '/api/admin', {
     method: 'GET',
   })
@@ -22,7 +23,7 @@ export default async function AdminLayout() {
     <>
       {res && <h1>{res.message + res.success}</h1>}
       {!res && <h1>why no response</h1>}
-      {process.env.VERCEL_URL && <h1>VERCEL_URL: {process.env.VERCEL_URL}</h1>}
+      {url && <h1>{url}</h1>}
       <Sales res={res} />
 
       <LoginHistory />
