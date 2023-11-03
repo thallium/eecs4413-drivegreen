df6c8424ee147e0d3dcaa0c42457d52e8dc416ee
Gengchen <tuogengchen@gmail.com>
2023-11-02 21:03:37 -0400

    modify db seed file

--- prisma/seedUser.mjs
+++ prisma/seedUser.mjs
@@ -1,24 +1,13 @@
 export default async function seedUser(prisma) {
   const user1 = await prisma.user.upsert({
-    where: { email: 'aaa@gmail.com', },
+    where: { email: 'user1@gmail.com', },
     update: {},
     create: { // minimum: only required fields
-      uid: 'a1234',
-      name: 'aaa',
-      email: 'aaa@gmail.com'
+      uid: 'd2ikbaUUmZRi9d7KKhNT6cQZ4EY2',
+      name: 'Tony',
+      email: 'user1@gmail.com'
     },
   })
 
-  const user2 = await prisma.user.upsert({
-    where: { email: 'bbb@gmail.com', },
-    update: {},
-    create: { // minimum: only required fields
-      uid: 'b1234',
-      name: 'bbb',
-      email: 'bbb@gmail.com'
-    },
-  })
-
-  console.log({ user1, user2 })
-  return [user1, user2]
+  return [user1]
 }
