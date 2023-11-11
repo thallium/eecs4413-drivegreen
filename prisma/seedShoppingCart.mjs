9ae5e3e521344a430b1851e200eeff553c518ddd
Lu Shen <853166476@qq.com>
2023-11-10 22:28:06 -0500

    shopping cart info fetching and front-end showing

--- prisma/seedShoppingCart.mjs
+++ prisma/seedShoppingCart.mjs
@@ -39,33 +39,40 @@ export default async function seedShoppingCart(prisma) {
     },
   })
 
-  // const sc2 = await prisma.shoppingCart.upsert({
-  //   where: { userId: user2.uid, },
-  //   update: {},
-  //   create: {
-  //     user: {
-  //       connect:{
-  //         uid: user2.uid
-  //       }, 
-  //     },
-  //     vehicleItems: {
-  //       create: [
-  //         {
-  //           vehicle: {
-  //             connect: { vid: veh2.vid }, 
-  //           },
-  //           quantity: 1, 
-  //           subTotal: 1 * veh2.price,
-  //         }
-  //       ],
-  //     },
-  //     totalPrice: 1 * veh2.price,
-  //   },
-  //   include: {
-  //     vehicleItems: true
-  //   },
-  // })
+  const sc2 = await prisma.shoppingCart.upsert({
+    where: { userId: user2.uid, },
+    update: {},
+    create: {
+      user: {
+        connect:{
+          uid: user2.uid
+        }, 
+      },
+      vehicleItems: {
+        create: [
+          {
+            vehicle: {
+              connect: { vid: veh2.vid }, 
+            },
+            quantity: 1, 
+            subTotal: 1 * veh2.price,
+          },
+          {
+            vehicle: {
+              connect: { vid: veh1.vid }, 
+            },
+            quantity: 2, 
+            subTotal: 2 * veh1.price,
+          }
+        ],
+      },
+      totalPrice: 1 * veh2.price + 2 * veh1.price,
+    },
+    include: {
+      vehicleItems: true
+    },
+  })
 
-  return [sc1]
+  return [sc1, sc2]
 
 }
