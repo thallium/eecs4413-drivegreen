7a359ef55996174fe319e987d4e66b70e2509dc6
sss <10131931+sowiesss@users.noreply.github.com>
2023-11-15 19:38:21 -0500

    fix seedLH

--- prisma/seedLoginHistory.mjs
+++ prisma/seedLoginHistory.mjs
@@ -1,49 +1,50 @@
-
+import seedUser from './seedUser.mjs';
 
 export default async function seedLoginHistory(prisma) {
-    const h1 = await prisma.loginHistory.upsert({
-      where: {
-        userId_loginAt: {
-          userId: 'd2ikbaUUmZRi9d7KKhNT6cQZ4EY2',
-          loginAt: new Date('2023-11-01T12:00:00Z'),
-        },
-      },
-      update: {},
-      create: {
-        ip: '101.0.0.1',
+  const [user1, user2, user3] = await seedUser(prisma);
+  const h1 = await prisma.loginHistory.upsert({
+    where: {
+      userId_loginAt: {
+        userId: user2.uid,
         loginAt: new Date('2023-11-01T12:00:00Z'),
-        user: { connect: { uid: 'd2ikbaUUmZRi9d7KKhNT6cQZ4EY2' } },
       },
-    });
+    },
+    update: {},
+    create: {
+      ip: '101.0.0.1',
+      loginAt: new Date('2023-11-01T12:00:00Z'),
+      user: { connect: { uid: user2.uid } },
+    },
+  });
 
-    const h2 = await prisma.loginHistory.upsert({
-      where: {
-        userId_loginAt: {
-          userId: 'b1234',
-          loginAt: new Date('2023-11-11T12:00:05Z'),
-        },
-      },
-      update: {},
-      create: {
-        ip: '101.0.2.0',
+  const h2 = await prisma.loginHistory.upsert({
+    where: {
+      userId_loginAt: {
+        userId: user3.uid,
         loginAt: new Date('2023-11-11T12:00:05Z'),
-        user: { connect: { uid: 'b1234' } },
       },
-    });
+    },
+    update: {},
+    create: {
+      ip: '101.0.2.0',
+      loginAt: new Date('2023-11-11T12:00:05Z'),
+      user: { connect: { uid: user3.uid } },
+    },
+  });
 
-    const h3 = await prisma.loginHistory.upsert({
-      where: {
-        userId_loginAt: {
-          userId: 'b1234',
-          loginAt: new Date('2023-11-04T10:00:00Z'),
-        },
-      },
-      update: {},
-      create: {
-        ip: '101.0.2.0',
+  const h3 = await prisma.loginHistory.upsert({
+    where: {
+      userId_loginAt: {
+        userId: user3.uid,
         loginAt: new Date('2023-11-04T10:00:00Z'),
-        user: { connect: { uid: 'b1234' } },
       },
-    });
-    console.log("login history seed:", h1, h2, h3);
+    },
+    update: {},
+    create: {
+      ip: '101.0.2.0',
+      loginAt: new Date('2023-11-04T10:00:00Z'),
+      user: { connect: { uid: user3.uid } },
+    },
+  });
+  console.log('login history seed:', h1, h2, h3);
 }
--- src/app/components/admin/Sales.jsx
+++ src/app/components/admin/Sales.jsx
@@ -2,18 +2,15 @@
 import {sale_reports } from '@/app/backend/service/admin/reports';
 
 
-export const getServerSideProps = async () => {
+const getSales = async () => {
  
   const sales = await sale_reports();
   
-  return {
-    props: {
-      sales: sales,
-    },
-  };
+  return sales;
 };
 
-export default function Sales({sales}) {
+export default async function Sales() {
+  const sales = await getSales();
   
   return (
     <div className="overflow-x-auto max-h-48 overflow-y-scroll border-solid border-2 rounded border-grey-400">
