01a873b26a8c8a092f2a605ac2f530b5d0008a9b
Gengchen <tuogengchen@gmail.com>
2023-11-17 15:59:33 -0500

    add profile api

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,28 @@
+import { prisma } from "@/app/backend/db/dbClient";
+/**
+ * @param {Request} request
+ */
+export async function POST(request) {
+    const session = await getServerSession();
+    if (session) {
+        try {
+            const req = await request.json();
+            const email = req.email;
+            const res = await prisma.user.update({
+                where: {
+                    email: email
+                },
+                data: {
+                    name: req.name,
+                    email: email,
+                    address: req.address,
+                },
+            })
+            return NextResponse.json(res, { status: 200 });
+        } catch (error) {
+            return new NextResponse(error.message, { status: 500 });
+        }
+    } else {
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+}
--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,21 +1,14 @@
 'use client';
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Example() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const router = useRouter();
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        className="mx-auto h-10 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="Your Company"
-                    />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
