2f1f9ef6b3a9acf1425e1d1b9e3cb7c91e6413ea
Gengchen <tuogengchen@gmail.com>
2023-11-18 20:58:33 -0500

    add watch list api and check at login

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,10 +1,30 @@
 'use client';
 import { signIn } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 import { useState } from 'react';
+import { baseURL } from '@/util';
+import { useRouter } from 'next/navigation';
+
+async function doSignIn(email, password) {
+    const res = await signIn('credentials', { email, password, redirect: false })
+    if (res.ok) {
+        const res = await fetch(baseURL() + '/api/watchlist/check')
+        const hotdeal = await res.json()
+        if (hotdeal.length >= 0) {
+            const cars = hotdeal.join(', ')
+            alert(`The ${cars} you're watching is in a hot deal! Check it out!`)
+        }
+        return true;
+    } else {
+        alert('Wrong email or password');
+        return false;
+    }
+}
 
 export default function Example() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const router = useRouter()
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -60,7 +80,11 @@ export default function Example() {
                         <div>
                             <button
                                 type='button'
-                                onClick={() => signIn('credentials', { email, password, redirect: true, callbackUrl: '/' })}
+                                onClick={async () => {
+                                    if (await doSignIn(email, password)) {
+                                        router.push("/")
+                                    }
+                                }}
                                 disabled={!email || !password}
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
